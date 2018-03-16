c0897bd406094ae1f3c273aa055d3b29dd896b58
emptychan <chen19941018@live.com>
2018-03-16 18:18:19 +0800

    C4.5缺失值尝试-1

---
+++ machine_learning/cart_decision_tree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+"""
+ Created with IntelliJ IDEA.
+ Description:
+ User: jinhuichen
+ Date: 3/9/2018 11:38 AM 
+ Description: https://www.2cto.com/net/201711/696334.html
+              http://blog.csdn.net/acdreamers/article/details/44664481
+              http://www.cnblogs.com/zhangchaoyang/articles/2709922.html
+              随机森林（未来）
+              https://zhuanlan.zhihu.com/p/22097796
+"""
---
+++ machine_learning/utils.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- 
+"""
+ Created with IntelliJ IDEA.
+ Description:
+ User: jinhuichen
+ Date: 3/12/2018 5:39 PM 
+ Description: 
+"""
